eeedac1c49552234c2b2ff31f702d0c7ce841cd4
fallonloic <fallonloic@hotmail.fr>
2020-04-05 15:04:28 +0200

    allow access ?

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ var moneyRouter = require('./routes/money');
 
 var app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+    // allowed XHR methods
+    res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+    res.send();
+  });
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
